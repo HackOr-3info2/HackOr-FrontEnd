f0da524b8171c8ec3c706045954452ef63aa5d06
horodeski <geovana.horodeski06@gmail.com>
2023-11-23 09:21:54 -0300

    Alterações no settings

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,13 +1,32 @@
+<script setup></script>
+
 <template>
-  s
+  <main>
+    <h2>Configurações</h2>
+    <div class="header">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, eaque autem quia itaque soluta
+        ullam ut. Maxime soluta provident eligendi asperiores autem eaque fugit eos, delectus
+        veniam, accusamus aperiam in.
+      </p>
+    </div>
+    <section class="pessoais">
+      <h3>Informações pessoais</h3>
+      
+    </section>
+  </main>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+main {
+  padding: 5em 20em;
 }
-</script>
-
-<style>
-
+.header {
+  border: 4px solid var(--primary-color-lg);
+  padding: 2em;
+  border-radius: var(--border-rds);
+}
+.pessoais {
+  margin-top: 2em
+}
 </style>
